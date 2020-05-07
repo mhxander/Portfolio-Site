023d9b4b1c2185974269dae66657bf9337c93686
Michael <mhxander@users.noreply.github.com>
2020-05-07 13:51:38 -0400

    Updated 1
    Added pictures. Started working on error messages.  Got most links working.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const data = require('./data.json');
 
-app.use('/static', express/static('public'));
+app.use('/static', express.static('public'));
 
 app.set('view engine', 'pug');
 
